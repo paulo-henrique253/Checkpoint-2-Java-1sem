4acb0761e564652e33ff587508a1249dfafa2b20
jacquelot <jacquelinelotuffo@hotmail.com>
2026-05-17 19:05:15 -0300

    feat: classe Veiculo

---
+++ uber/src/br/com/tdspk/veiculo/Veiculo.java
@@ -0,0 +1,47 @@
+package br.com.tdspk.veiculo;
+
+public class Veiculo {
+    private String placa;
+    private String modelo;
+    private String montadora;
+    private int ano;
+
+    public Veiculo(String placa, String modelo, String montadora, int ano) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.montadora = montadora;
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMontadora() {
+        return montadora;
+    }
+
+    public void setMontadora(String montadora) {
+        this.montadora = montadora;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
