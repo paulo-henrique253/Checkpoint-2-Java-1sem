98200ef7426ee5d8cde777f53894672e20f110fe
jacquelot <jacquelinelotuffo@hotmail.com>
2026-05-17 18:41:42 -0300

    feat: classe cliente

---
+++ uber/src/br/com/tdspk/cliente/Cliente.java
@@ -0,0 +1,40 @@
+package br.com.tdspk.cliente;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String celular;
+
+    public Cliente() {
+
+    }
+    public Cliente (String nome,String cpf, String celular) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.celular = celular;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+}
